95397af291af4acca2b12ecff835132dbea1358b
Baj140316 <116079624+Baj140316@users.noreply.github.com>
2023-11-24 11:09:29 +0100

    Update Query-Logs.sql

--- Scripts/Query-Logs.sql
+++ Scripts/Query-Logs.sql
@@ -1,3 +1,6 @@
+-- Um Query-Logs zu entleeren ohne zu Deaktivieren 
+-- TRUNCATE mysql.general_log
+  
 -- Aktivere die Logs
 SET global general_log = 1;
 
@@ -6,6 +9,3 @@ SET global log_output = 'table';
 
 -- Output von den Query-Logs
 select * from mysql.general_log;
-
--- Um Query-Logs zu entleeren ohne zu Deaktivieren 
-TRUNCATE mysql.general_log
