da42369e54c6b05a6b29115b4d58c648906a1307
DRKTRZY <94437473+DRKTRZY@users.noreply.github.com>
2023-11-24 11:08:14 +0100

    Create Query-Logs.sql

---
+++ Scripts/Query-Logs.sql
@@ -0,0 +1,11 @@
+-- Aktivere die Logs
+SET global general_log = 1;
+
+-- Wo die Query-Logs gespeichert werden
+SET global log_output = 'table';
+
+-- Output von den Query-Logs
+select * from mysql.general_log;
+
+-- Um Query-Logs zu entleeren ohne zu Deaktivieren 
+TRUNCATE mysql.general_log
