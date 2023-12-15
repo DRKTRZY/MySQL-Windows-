2509b4dac0a59154d65b8468d48b71d1e08752f4
Baj140316 <116079624+Baj140316@users.noreply.github.com>
2023-12-15 08:57:48 +0100

    Update Create-Table.sql
    script korriegiert 
    primär schlüssel waren falsch

--- Scripts/Create-Table.sql
+++ Scripts/Create-Table.sql
@@ -1,64 +1,69 @@
--- Erstelle die Datenbank "frigg"
-DROP DATABASE IF EXISTS frigg;
-CREATE DATABASE frigg;
- 
--- Verwende die Datenbank "frigg" für alle folgenden Operationen
-USE frigg;
- 
--- Erstelle die Tabelle "Type"
-CREATE TABLE Type (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- Eindeutige ID für den Datensatz
-    name VARCHAR(100) -- Name der Art
-);
- 
--- Erstelle die Tabelle "Data"
-CREATE TABLE Data (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- Eindeutige ID für den Datensatz
-    digest CHAR(100), -- Digest-Wert für die Daten
-    size INT, -- Größe der Daten
-    compression INT, -- Art der Datenkompression
-    content Text(50000), -- Inhalt der Daten
-    type_id INT, -- Fremdschlüssel, der auf die Tabelle "Type" verweist
-    FOREIGN KEY (type_id) REFERENCES Type(id)
-);
- 
--- Erstelle die Tabelle "Tag"
-CREATE TABLE Tag (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- Eindeutige ID für den Tag
-    name VARCHAR(100) -- Name des Tags
-);
- 
--- Erstelle die Tabelle "Data_Tag" für die viele-zu-viele-Beziehung zwischen "Data" und "Tag"
-CREATE TABLE Data_Tag (
-    id int primary key auto_increment,
-    Tag_id INT,
-    Data_id INT,
-    FOREIGN KEY (Tag_id) REFERENCES Tag(id),
-    FOREIGN KEY (Data_id) REFERENCES Data(id)
-);
- 
--- Erstelle die Tabelle "systemuser" für Benutzerinformationen
-CREATE TABLE systemuser (
-    uid INT PRIMARY KEY AUTO_INCREMENT, -- Eindeutige ID für den Benutzer
-    name VARCHAR(20) -- Name des Benutzers
-);
- 
--- Erstelle die Tabelle "usergroup" für Benutzergruppen
-CREATE TABLE usergroup (
-    gid INT PRIMARY KEY AUTO_INCREMENT, -- Eindeutige ID für die Benutzergruppe
-    groupname VARCHAR(50) -- Name der Benutzergruppe
+drop database if exists frigg;
+create database frigg;
+
+use frigg;
+
+create table temp (
+    `#count` int not null unique primary key,
+    digest varchar(50) not null,
+    path text not null,
+    size int not null,
+    type nvarchar(50) not null,
+    permission int not null,
+    uid int not null,
+    user varchar(50) not null,
+    gid int not null,
+    `group` varchar(50) not null,
+    time int not null,
+    compression int not null,
+    data text not null
 );
 
--- Erstelle die Tabelle "Meta" für Metadaten
-CREATE TABLE Meta (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- Eindeutige ID für die Metadaten
-    path VARCHAR(500), -- Pfad zu den Daten
-    permission INT, -- Berechtigungen für die Daten
-    time INT, -- Zeitstempel für die Metadaten
-    usergroup_gid INT, -- Fremdschlüssel, der auf die Tabelle "usergroup" verweist
-    Data_id INT, -- Fremdschlüssel, der auf die Tabelle "Data" verweist
-    systemuser_uid INT, -- Fremdschlüssel, der auf die Tabelle "systemuser" verweist
-    FOREIGN KEY (usergroup_gid) REFERENCES usergroup(gid),
-    FOREIGN KEY (Data_id) REFERENCES Data(id),
-    FOREIGN KEY (systemuser_uid) REFERENCES systemuser(uid)
+create table type (
+    id int not null auto_increment primary key,
+    name varchar(50) unique
+);
+
+create table data (
+    digest varchar(50) not null primary key,
+    content text not null,
+    size int not null,
+    compression int not null,
+    type_id int,
+    foreign key (type_id) references type(id)
+);
+
+create table tag (
+    id int not null auto_increment primary key,
+    name varchar(50) unique
+);
+
+create table data_tag (
+    data_digest varchar(50) not null,
+    tag_id int,
+    primary key (data_digest, tag_id),
+    foreign key (data_digest) references data(digest),
+    foreign key (tag_id) references tag(id)
+);
+create table systemuser (
+    uid int not null unique primary key,
+    user varchar(50) not null unique
+);
+
+create table usergroup (
+    gid int not null unique primary key,
+    `groupname` varchar(50) not null unique
+);
+
+create table meta (
+    id int not null auto_increment primary key,
+    path varchar(250) not null,
+    perm int not null,
+    time int not null,
+    systemuser_uid int,
+    usergroup_gid int,
+    data_digest varchar(50) not null,
+    foreign key (systemuser_uid) references systemuser(uid),
+    foreign key (usergroup_gid) references usergroup(gid),
+    foreign key (data_digest) references data(digest)
 );
