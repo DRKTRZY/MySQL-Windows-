4c2dc6536d591c8f8f63cd03cdb57369e9899c90
Baj140316 <116079624+Baj140316@users.noreply.github.com>
2023-12-15 09:12:29 +0100

    Update Import.sql
    import script korrigiert statt temp manuell zu importieren ist es jz alles im script geht schneller

--- Scripts/Import.sql
+++ Scripts/Import.sql
@@ -1,18 +1,31 @@
-Use frigg;
+use frigg;
+ 
+load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.2\\Uploads\\temp.csv'
 
-Insert into frigg.type(name)
+into table `temp`
+
+fields terminated by ',' enclosed by '"'
+
+lines terminated by '\n'
+
+ignore 1 lines
+
+(`#count`, `digest`, `path`, `size`, `type`, `permission`, `uid`, `user`, `gid`, `groupname`, `time`, `compression`, `data`);
+ 
+select * from temp;
+
+use frigg;
+insert into frigg.type(name) 
 select distinct type from temp;
-
-Insert into frigg.systemuser(uid, name)
-select distinct uid, user from temp;
-
-Insert into frigg.usergroup(gid, groupname)
-select distinct gid, "group" from temp;
-
-Insert into frigg.data(digest, size, compression, content, type_id)
-select distinct digest, size, compression, "data", (select ID from frigg.type where type.name = temp.type) from temp;
-
-Insert into frigg.meta(path, time, usergroup_gid, Data_id, systemuser_uid)
-select distinct path, time, gid,(Select ID from frigg.data where "frigg.data(content)" = "temp.data"), uid from temp;
-
-Drop table frigg.temp;
+ 
+insert into frigg.usergroup(`gid`, `groupname`)
+select distinct gid, `groupname` from temp;
+ 
+insert into frigg.systemuser(`uid`, `user`)
+select distinct uid, `user` from temp;
+ 
+insert into frigg.data(`digest`, `content`, `size`, `compression`, `type_id`)
+select distinct `digest`, `data`, `size`, `compression`, (select id from frigg.type where type.name = temp.type) from temp;
+ 
+insert into frigg.meta(`path`, `permission`, `time`, `systemuser_uid`, `usergroup_gid`, `data_digest`)
+select distinct `path`, `permission`, `time`, `uid`, `gid`, `digest` from temp;
